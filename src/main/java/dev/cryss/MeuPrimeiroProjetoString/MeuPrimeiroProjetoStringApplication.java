aa3a4bced6dbd9c8f4994a113a38e79ab3f78fa3
Crystyan S. Santos <47446514+CrystyanSantos9@users.noreply.github.com>
2022-04-28 22:41:45 -0300

    primeiro commit

---
+++ src/main/java/dev/cryss/MeuPrimeiroProjetoString/MeuPrimeiroProjetoStringApplication.java
@@ -0,0 +1,13 @@
+package dev.cryss.MeuPrimeiroProjetoString;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoStringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoStringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cryss/MeuPrimeiroProjetoString/MeuPrimeiroProjetoStringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cryss.MeuPrimeiroProjetoString;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoStringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
